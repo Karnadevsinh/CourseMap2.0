2f6c2d2668cb45a4721e8a9547d7abe000bfd375
Karan <karnadev2468@gmail.com>
2024-05-18 15:29:42 +0530

    UI draft 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/Home';
+import Course from './components/courses/Course';
+import SelectedCourse from './components/selectedCourse/SelectedCourse';
+import LoginTerminal from './components/login/LoginTerminal';
+import Transition from './components/selectedCourse/Transition';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SelectedCourse /> */}
+      {/* <Home /> */}
+      {/* <Course /> */}
+      {/* <LoginTerminal /> */}
+      <Transition />
     </div>
   );
 }
